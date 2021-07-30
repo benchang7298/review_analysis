12e3c9532ef68ac2e49b259535d4158987ac65c8
Ben Chang <ben.chang@intel.com>
2021-07-30 17:14:12 +0800

    Add files via upload

--- read1.py
+++ read1.py
@@ -4,10 +4,16 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0: #
+        if count % 1000 == 0:
             print(len(data))
-print(len(data)) 
 
-print(data[0])
-print('-------------')
-print(data[1])
+
+print('read process compile total count is', len(data), 'data' )
+
+sum_len = 0
+for d in data:
+     sum_len = sum_len + len(d)
+     
+print('Avarge length of rreview is', sum_len / len(data))
+
+print('----------')
